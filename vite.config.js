b487aca4a61ec06d86ce2cc4feba61748ba9adcf
Yu Ju Chang <YuJu.Chang86@gmail.com>
2025-11-24 20:03:03 -0600

    updated base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: "https://github.com/changyuj/portfolio",
   plugins: [react()],
 });
